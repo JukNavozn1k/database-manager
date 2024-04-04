b68897624f91d9a33c06f499000f18a70d928a3b
JukNavozn1k <JukNavozn1k>
2024-04-04 06:47:38 -0700

    removed DeclarativeBase & added generic insert

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from models.schema import Base
+from models.config import MetaData
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = MetaData
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ models/crud.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Table
+from config import metadata,engine,Session
+
+
+def insert_into_table(table_name, **kwargs):
+   
+    table = Table(table_name, metadata, autoload=True)
+
+    with Session() as session:
+        # Create an insert statement
+        insert_statement = table.insert().values(**kwargs)
+        session.execute(insert_statement)
+        session.commit()
+    
+
+insert_into_table(table_name='category',id=66,name='shrek')
