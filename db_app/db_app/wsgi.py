dc5fcc33cb0d8ff5fa728be6a975f5169527b66b
JukNavozn1k <JukNavozn1k>
2024-04-05 11:13:46 -0700

    added app

--- db_app/db_app/wsgi.py
+++ db_app/db_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for db_manager project.
+WSGI config for db_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'db_manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'db_app.settings')
 
 application = get_wsgi_application()
