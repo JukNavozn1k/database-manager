757fa15568cdb2fc362149576493c643cc5054de
JukNavozn1k <JukNavozn1k>
2024-04-05 11:30:13 -0700

    added category

--- db_app/db_manager/models.py
+++ db_app/db_manager/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
+
+
+class PositiveFloatValidator(MinValueValidator):
+    def __init__(self, message=None):
+        super().__init__(0, message=message or 'Цена должна быть положительной')
+
+class Category(models.Model):
+      name = models.CharField(max_length=128,unique=True)
+      description = models.CharField(max_length=1024)
+      
+      def __str__(self) -> str:
+           return self.name
 # Create your models here.
+class Good(models.Model):
+      name = models.CharField(max_length=128,unique=True)
+      price = models.FloatField(validators=[PositiveFloatValidator()])
+
+      category = models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
+
+      def __str__(self) -> str:
+           return self.name
