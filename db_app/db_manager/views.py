df04cc37a9ecb17eddd413af53fa9bf964f02ebd
JukNavozn1k <JukNavozn1k>
2024-04-07 09:43:17 -0700

    table consructor

--- db_app/db_manager/views.py
+++ db_app/db_manager/views.py
@@ -11,57 +11,60 @@ from . import forms
     Very easy to add new endpoints, which can be convenient for asynchronous approach
 '''
 
+class AsyncTable:
+    def __init__(self,model,form,tablename) -> None:
+        self.model = model
+        self.form = form
+        self.tablename = tablename
+        pass
+   
+    def get_form(self,request):
+        form = self.form()
+ 
+        context = {'form' : form,'tablename':self.tablename}
+        return render(request,'form.html',context=context)
 
-@require_http_methods(["GET"])
-def get_form(request):
-    form = forms.GoodForm()
-    tablename = 'goods'
-    context = {'form' : form,'tablename':tablename}
-    return render(request,'form.html',context=context)
+
+    def get_table(self,request):
+        objects = self.model.objects.all()
+
+        fields = [field.name for field in self.model._meta.get_fields() if field.name != 'id']
+        fields_verbose = [field.verbose_name for field in self.model._meta.get_fields() if field.name != 'id']
+
+        context = {'fields': fields,'fields_verbose':fields_verbose,'objects':objects,'tablename':self.tablename}
+        return render(request,'table.html',context=context)
+
+    def search_table(self,request):
+        form = self.form(request.POST)
+        cleaned_data = {key : value for key,value in form.data.items() if value != ''}
+        objects = self.model.objects.filter(**cleaned_data)
+
+        fields = [field.name for field in self.model._meta.get_fields() if field.name != 'id']
+        fields_verbose = [field.verbose_name for field in self.model._meta.get_fields() if field.name != 'id']
+    
+        context = {'fields': fields,'fields_verbose':fields_verbose,'objects':objects,'tablename':self.tablename}
+        return render(request,'table.html',context=context)
+
+    
+    def delete_record(self,request,id):
+        object = self.model.objects.get(id=id)
+        object.delete()
+        return HttpResponse('')
 
 
-@require_http_methods(["GET","POST"])
-def get_table(request):
-    objects = models.Good.objects.all()
+    def add_record(self,request):
+            object_form = self.form(request.POST)
+            if object_form.is_valid():
+                object_form.save()
+                return self.get_table(request)
+            else: return HttpResponse(f'''
+            <div class="alert alert-danger" role="alert">
+            <h4 class="alert-heading">Ошибка!</h4>
+            <hr>
+            <p class="mb-0">{object_form.errors}</p>
+            </div>
+            ''')
 
-    fields = [field.name for field in models.Good._meta.get_fields() if field.name != 'id']
-    fields_verbose = [field.verbose_name for field in models.Good._meta.get_fields() if field.name != 'id']
-  
-    tablename = 'goods'
-    context = {'fields': fields,'fields_verbose':fields_verbose,'objects':objects,'tablename':tablename}
-    return render(request,'table.html',context=context)
-
-@require_http_methods(["POST"])
-def search_table(request):
-    form = forms.GoodForm(request.POST)
-    cleaned_data = {key : value for key,value in form.data.items() if value != ''}
-    objects = models.Good.objects.filter(**cleaned_data)
-
-    fields = [field.name for field in models.Good._meta.get_fields() if field.name != 'id']
-    fields_verbose = [field.verbose_name for field in models.Good._meta.get_fields() if field.name != 'id']
-    tablename = 'goods'
-    context = {'fields': fields,'fields_verbose':fields_verbose,'objects':objects,'tablename':tablename}
-    return render(request,'table.html',context=context)
-
-@require_http_methods(["DELETE"])
-def delete_record(request,id):
-       object = models.Good.objects.get(id=id)
-       object.delete()
-       return HttpResponse('')
-
-@require_http_methods(["POST"])
-def add_record(request):
-        good = forms.GoodForm(request.POST)
-        if good.is_valid():
-            good.save()
-            return get_table(request)
-        else: return HttpResponse(f'''
-        <div class="alert alert-danger" role="alert">
-        <h4 class="alert-heading">Ошибка!</h4>
-        <hr>
-        <p class="mb-0">{good.errors}</p>
-        </div>
-        ''')
 
 
 '''
@@ -75,6 +78,7 @@ class Home(View):
 
 class GoodsManager(View):
     def get(self,request):   
-        good_form = forms.GoodForm()
-        context = {'good_form':good_form}
-        return render(request,'manager.html',context=context)
+       
+        return render(request,'manager.html')
+
+GoodsTable = AsyncTable(models.Good,forms.GoodForm,'goods')
